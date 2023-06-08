d4cd742cf1c6966f1020400b956785ba2e20eae7
Brady Six <bpsix646@gmail.com>
2023-06-08 11:25:19 -0700

    Add test case skeletons

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -18,14 +18,53 @@ public class CILabTest {
         myString = null;
     }
 
+    //Null string case
+    @Test
+    public void setStringTest1() {
+        fail("TODO");
+    }
+
+    //Average string case
+    @Test
+    public void setStringTest2() {
+        fail("TODO");
+    }
+
+    //Null string case
+    @Test
+    public void getStringTest1() {
+        fail("TODO");
+    }
+
+    //Average string case
+    @Test
+    public void getStringTest2() {
+        fail("TODO");
+    }
+
+    //All lowercase characters case
     @Test
     public void detectCapitalUseTest1() {
        fail("Not yet implemented");
 
     }
+
+    //Only first letter capitalized case
     @Test
     public void detectCapitalUseTest2() {
         fail("Not yet implemented");
+    }
+
+    //All capital letters case
+    @Test
+    public void detectCapitalUseTest3() {
+        fail("TODO");
+    }
+
+    //Contains non-letter characters case
+    @Test
+    public void detectCapitalUseTest4() {
+        fail("TODO");
     }
 
 
